263be2569b7ddec1510f174abf4354ae02966bf1
Shawn Shin <shawnshin@mac.ad.medctr.ucla.edu>
2025-08-12 13:22:23 -0700

    Remove build directory from Git tracking

--- build/CMakeFiles/droplet.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/droplet"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/droplet.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
